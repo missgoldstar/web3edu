381bbff429f9c664e78f13b51667ed156d3196ca
missgoldstar <missgoldstar@vdlabs.io>
2024-06-22 22:18:11 +0900

    first commit

---
+++ contracts/NFT_V2.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721, Ownable {
+    string public baseUri;
+    string public fileExtention = ".json";
+    using Strings for uint256;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _baseuri
+    ) ERC721(_name, _symbol) {
+        setBaseURI(_baseuri);
+    }
+
+    function setBaseURI(string memory _uri) public {
+        baseUri = _uri;
+    }
+
+    function baseURI() external view returns (string memory) {
+        return baseUri;
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        // string memory baseURI = baseURI();
+        return
+            bytes(baseUri).length > 0
+                ? string(
+                    abi.encodePacked(baseUri, tokenId.toString(), fileExtention)
+                )
+                : "";
+    }
+}
